cbccbf3a8ebb0814323461be82357af8ba5aff60
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-10 21:31:03 -0400

    first

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+    function myFunction(event) { 
+        event.preventDefault();
+        console.log(document.getElementById("inputUsername").value);
+        console.log(document.getElementById("inputPassword4").value);
+        console.log(document.getElementById("inputFirstName").value);
+        console.log(document.getElementById("inputLastName").value);
+        console.log(document.getElementById("inputPhone").value);
+        console.log(document.getElementById("inputEmail").value);
+    };
